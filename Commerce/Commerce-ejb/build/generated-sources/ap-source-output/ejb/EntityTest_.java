da69fae56e800a0776dcd622203a636a5fd9348b
Aymeric <aymeric.delecourt@grenoble-inp.org>
2016-11-01 17:35:37 +0100

    Version qui fonctionne

---
+++ Commerce/Commerce-ejb/build/generated-sources/ap-source-output/ejb/EntityTest_.java
@@ -0,0 +1,15 @@
+package ejb;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(EntityTest.class)
+public abstract class EntityTest_ {
+
+	public static volatile SingularAttribute<EntityTest, String> toto;
+	public static volatile SingularAttribute<EntityTest, Long> id;
+
+}
+
